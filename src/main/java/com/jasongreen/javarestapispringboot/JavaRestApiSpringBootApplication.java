cb6613d89dc382472c14e914b7fd6dedde2dca8c
jgreen44 <jgreen44@asu.edu>
2021-09-04 15:36:08 -0500

    initial commit

---
+++ src/main/java/com/jasongreen/javarestapispringboot/JavaRestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jasongreen.javarestapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasongreen/javarestapispringboot/JavaRestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasongreen.javarestapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
